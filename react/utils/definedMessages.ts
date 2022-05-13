af4bdf9ac562e883890c854089b390716e8f80ce
Ana Maia <ana.baptistao@vtex.com.br>
2022-05-13 09:55:17 -0300

    messages

---
+++ react/utils/definedMessages.ts
@@ -0,0 +1,35 @@
+import { defineMessages } from "react-intl"
+
+export const messages = defineMessages({
+    title: {
+      id: 'admin/interface.title',
+    },
+    active:{
+      id: 'admin/interface.active',
+    },
+    disabled:{
+      id: 'admin/interface.disabled',
+    }
+  })
+  
+export const columns = defineMessages({
+    title: {
+      id: 'admin/interface.colums.title',
+    },
+    user: {
+      id: 'admin/interface.colums.user',
+    },
+    validate: {
+      id: 'admin/interface.colums.validate',
+    },
+    bought: {
+      id: 'admin/interface.colums.bought',
+    },
+    converted: {
+      id: 'admin/interface.colums.converted',
+    },
+    status: {
+      id: 'admin/interface.colums.status',
+    },
+  })
+  
