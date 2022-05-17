9b34fe73f3b5a922e563dba98a7a35b81f86946d
Ana Maia <ana.baptistao@vtex.com.br>
2022-05-17 13:26:49 -0300

    utils and typings

---
+++ react/typings/valuesLists.d.ts
@@ -0,0 +1,26 @@
+interface ValuesLists{
+    id: string
+    listType: string
+    name: string
+    active: boolean
+    public: boolean
+    otherName: string
+    ownerName: string
+    ownerEmail: string
+    image: string
+    backgroundImage: string
+    quantityPurchased: number
+    valuePurchased: number
+    eventDate: string
+    createdIn: string
+}
+
+interface Items{
+    id: string;
+    title: string;
+    user: string;
+    validate: number;
+    bought: number;
+    converted: number;
+    status: boolean;
+}
