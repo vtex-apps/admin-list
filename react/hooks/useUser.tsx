1ec6e38fc3398ef0bb2f9ab124666cd906a956b6
Ana Maia <ana.baptistao@vtex.com.br>
2022-06-01 14:51:58 -0300

    provider user

---
+++ react/hooks/useUser.tsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+
+interface ContextUser {
+  gridUsers: any 
+  view: any
+}
+
+export const ContextUser = React.createContext<ContextUser>({
+  gridUsers: {},
+  view: {},
+})
+
+export function useUser() {
+  return useContext(ContextUser)
+}
