121ecd3e25c277d399ca2a6edb495f4485720597
Ana Maia <ana.baptistao@vtex.com.br>
2022-06-01 14:50:37 -0300

    front interface

--- react/components/TableUser.tsx
+++ react/components/TableUser.tsx
@@ -9,15 +9,14 @@ import {
   Search,
   usePaginationState,
   useSearchState,
-  
 } from '@vtex/admin-ui'
-import { useLists } from '../hooks/useLists'
 import { ITEMS_PER_PAGE } from '../utils/constants'
 import { table } from '../utils/definedMessages'
 import { useIntl } from 'react-intl'
+import { useUser } from '../hooks/useUser'
 
-const TableArea: FC = () => {
-  const { gridLists, view } = useLists()
+const TableUserArea: FC = () => {
+  const { gridUsers, view } = useUser()
   const search = useSearchState()
   
   const {formatMessage} = useIntl()
@@ -26,7 +25,7 @@ const TableArea: FC = () => {
     total: 45,
   })
 
-  if(Object.keys(gridLists).length < 1 && Object.keys(view).length < 1) return <></>
+  if(Object.keys(gridUsers).length < 1 && Object.keys(view).length < 1) return <></>
 
   return ( 
     <div style={{ padding: '0 4rem' }}>
@@ -42,10 +41,10 @@ const TableArea: FC = () => {
             nextLabel={formatMessage(table.paginationNext)}
           />
         </DataViewControls>
-        <DataGrid state={gridLists} />
+        <DataGrid state={gridUsers} />
       </DataView> 
     </div>
   )
 }
 
-export default TableArea
+export default TableUserArea
