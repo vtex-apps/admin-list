e0a817e12b4d914abd641475b4a8a12d5735f3a9
Ana Maia <ana.baptistao@vtex.com.br>
2022-06-02 18:39:43 -0300

    typings

--- react/typings/valuesLists.d.ts
+++ react/typings/valuesLists.d.ts
@@ -1,26 +1,34 @@
-interface ValuesLists{
-    id: string
-    listType: string
-    name: string
-    active: boolean
-    public: boolean
-    otherName: string
-    ownerName: string
-    ownerEmail: string
-    image: string
-    backgroundImage: string
-    quantityPurchased: number
-    valuePurchased: number
-    eventDate: string
-    createdIn: string
+interface ValuesLists {
+  id: string
+  listType: string
+  name: string
+  active: boolean
+  public: boolean
+  otherName: string
+  ownerName: string
+  ownerEmail: string
+  image: string
+  backgroundImage: string
+  quantityPurchased: number
+  valuePurchased: number
+  eventDate: string
+  createdIn: string
 }
 
-interface ItemsLists{
-    id: string;
-    title: string;
-    user: string;
-    validate: number;
-    bought: number;
-    converted: number;
-    status: boolean;
+interface ItemsLists {
+  id: string
+  title: string
+  validate: number
+  bought: number
+  status: boolean
+}
+
+interface TotalizerListsUsers {
+  ownerName: string
+  ownerEmail: string
+  lists: number
+  purchase: number
+  quantityAlreadyInGiftCard: number | undefined
+  status: boolean
+  id: string
 }
