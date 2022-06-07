1633ed57c6ba41d1f092465af54ba7c163df76cf
Ana Maia <ana.baptistao@vtex.com.br>
2022-06-07 16:12:26 -0300

    searh user

--- react/hooks/useUser.tsx
+++ react/hooks/useUser.tsx
@@ -3,11 +3,17 @@ import React, { useContext } from 'react'
 interface ContextUser {
   gridUsers: any
   view: any
+  search: string
+  getInputProps: any
+  debouncedValue: string
 }
 
 export const ContextUser = React.createContext<ContextUser>({
   gridUsers: {},
   view: {},
+  search: '',
+  getInputProps: () => {},
+  debouncedValue: '',
 })
 
 export function useUser() {
