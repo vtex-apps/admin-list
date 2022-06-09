823382bbed0ff4626478651cfad9cc98daf7c240
Ana Maia <ana.baptistao@vtex.com.br>
2022-06-09 10:11:12 -0300

    Remove Status

--- react/components/Title.tsx
+++ react/components/Title.tsx
@@ -1,33 +1,19 @@
 import type { FC } from 'react'
 import React from 'react'
-import { Tag } from '@vtex/admin-ui'
-import { useIntl } from 'react-intl'
 
 import { useInterface } from '../hooks/useInterface'
-import { messages } from '../utils/definedMessages'
 import '../styles.global.css'
 
 const TitleArea: FC = () => {
   const { infoUserList } = useInterface()
 
-  const { formatMessage } = useIntl()
-
   return (
-    <div className="parent">
-      <div>
-        <p>
-          <b>{infoUserList?.ownerName}</b>
-          <br />
-          {infoUserList?.ownerEmail}
-        </p>
-      </div>
-      <div>
-        {infoUserList?.status ? (
-          <Tag label={formatMessage(messages.active)} palette="green" />
-        ) : (
-          <Tag label={formatMessage(messages.disabled)} palette="red" />
-        )}
-      </div>
+    <div>
+      <p>
+        <b>{infoUserList?.ownerName}</b>
+        <br />
+        {infoUserList?.ownerEmail}
+      </p>
     </div>
   )
 }
